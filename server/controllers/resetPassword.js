da60c82691da7ebae7387619e0c59b787fb5ae64
Omjha23 <91455041+Omjha23@users.noreply.github.com>
2024-06-27 16:01:28 +0530

    Update resetPassword.js

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -26,7 +26,8 @@ exports.resetPasswordToken = async (req, res) => {
 
     // const url = `http://localhost:3000/update-password/${token}`
     
-    const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
+   
+    const url = `https://studynotion-my.vercel.app//update-password/${token}`
 
     await mailSender(
       email,
